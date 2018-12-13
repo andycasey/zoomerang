37fd472da4cfac7ad34676b083bd6ca338bda00b
Andy Casey <andycasey@gmail.com>
2018-12-13 15:59:44 +1100

    add scheduler

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
 zoomerang
----------
+=========
 
 Because my sleep is important and Australia's time zone *sucks*.
+
+Scheduler
+---------
+
+The Scheduler looks through my Google Calendar for upcoming Zoom meetings that I want Zoomerang to record,
+and schedules a `cron` job to record the meeting. It's a good idea to run the Scheduler at 23:59 on Sundays.
+The following `cron` job will do that for you:
+
+``59 23 * * 6 python scheduler.py``
+
+
+Zoomerang
+---------
+
+The Zoomerang script does the following:
+
+1. Calls in and records a specified Zoom meeting.
+
+2. Downloads the audio MP3 of that Zoom meeting.
+
+3. Updates the `meetings.yaml` file with information about the recorded meeting.
+
+4. Updates the `meetings.xml` file, which is a RSS Podcast feed that my phone checks daily for updates.
---
+++ scheduler.py
@@ -0,0 +1,64 @@
+
+import datetime
+
+from googleapiclient.discovery import build
+from oauth2client import file, client, tools
+from httplib2 import Http
+
+
+def get_calendar():
+    
+    store = file.Storage('token.json')
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets(
+            'credentials.json', 
+            'https://www.googleapis.com/auth/calendar.readonly')
+        creds = tools.run_flow(flow, store)
+    return build('calendar', 'v3', http=creds.authorize(Http()))
+
+
+def find_upcoming_zoomerang_events(calendar, days_ahead=7):
+    now = datetime.datetime.utcnow()
+    time_min = now.isoformat() + "Z"
+    time_max = (now + datetime.timedelta(days=days_ahead)).isoformat() + "Z"
+
+    results = calendar.events().list(calendarId="primary", 
+                                     timeMin=time_min,
+                                     timeMax=time_max, 
+                                     maxResults=100,
+                                     singleEvents=True,
+                                     orderBy="startTime").execute()
+    events = results.get("items", [])
+
+    zoomerang_events = []
+    for event in events:
+        if event.get("location", "").lower().startswith("zoomerang"):
+            zoomerang_events.append(event)
+
+    return zoomerang_events
+
+
+def format_cron_job(zoomerang_event):
+
+    start_datetime = zoomerang_events[0]["start"]["dateTime"]
+    start_datetime = ":".join(start_datetime.split(":")[:-1]) \
+                   + start_datetime[-2:]
+    meeting_id = zoomerang_event["location"].split()[1]
+    summary = zoomerang_event.get("summary", f"Meeting {meeting_id}").replace('"', '')
+
+    st = datetime.datetime.strptime(start_datetime, "%Y-%m-%dT%H:%M:%S%z")
+
+    return f"{st.minute} {st.hour} {st.day} {st.month} * python zoomerang.py {meeting_id} \"{summary}\""
+
+
+
+if __name__ == '__main__':
+
+    zoomerang_events = find_upcoming_zoomerang_events(get_calendar())
+    cron_jobs = "\n".join([format_cron_job(e) for e in zoomerang_events]) + "\n"
+
+    #/etc/cron.d/zoomerang
+    with open("zoomerang", "w") as fp:
+        fp.write(cron_jobs)
+
